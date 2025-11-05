2bc39cd8dc84f854085f297d33bc00a8d39daa84
ryousuke203 <ryousuke.20454@gmail.com>
2025-11-05 22:00:41 +0900

    イベントの発生マネージャー途中

--- Uminorikyu/Assets/ATMembers/orityan/Event/EventSpawner.cs
+++ Uminorikyu/Assets/ATMembers/orityan/Event/EventSpawner.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Threading;
 using UnityEditor.XR;
 using UnityEngine;
 
@@ -7,15 +8,14 @@ public class EventSpawner : MonoBehaviour
     [System.Serializable]
     public struct EventState
     {
-        [SerializeField] public GameObject events;
+        public GameObject events;
         public float percent;
         public bool appearance;
     }
 
     [SerializeField] EventState[] eventStates;
     RoundTimer timer;
-    float seed;
-    int rand;
+    int eventCount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,9 +31,30 @@ public class EventSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < eventStates.Count(); i++)
+        {
+            if (eventStates[i].appearance)
+            {
+                eventCount++;
+            }
+        }
 
-
-        
+        if (eventCount < 3)
+        {
+            if (timer.nowTime % 5 == 0)
+            {
+                for (int i = 0; i < eventStates.Count(); i++)
+                {
+                    if (!eventStates[i].appearance)
+                    {
+                        if (Random.Range(1, 100) < eventStates[i].percent)
+                        {
+                            Instantiate(eventStates[i].events);
+                            eventStates[i].appearance = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 }

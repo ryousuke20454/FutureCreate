e3774d16f8c48ddeb2543a2a018d90ee675c7573
taiga-0214 <taiga.kaneko214@gmail.com>
2025-11-13 09:44:26 +0900

    フェード追加

---
+++ Uminorikyu/Assets/ATMembers/taityan/Script/BackWave.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class BackWave : MonoBehaviour
+{
+    public RectTransform image; // 動かす画像の参照
+    private int counter = 0; // カウンター変数
+    private float move = 1.0f; //この数字の2倍動きます
+
+    private float num = 860.0f;//端から端までの移動量
+
+    bool canmove = true;//波を表示してうごかすかどうか
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (canmove)
+        {
+            image.position += new Vector3(move, 0, 0);
+            num -= move;
+        }
+
+
+        // カウンターが一定値に達したら方向を反転
+        if (num <= 0)
+        {
+            canmove = false;
+        }
+    }
+}
--- Uminorikyu/Assets/ATMembers/taityan/Script/NextScene.cs
+++ Uminorikyu/Assets/ATMembers/taityan/Script/NextScene.cs
@@ -1,56 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
-//using UnityEngine.UIElements;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class NextScene:MonoBehaviour
 {
-    ////自身の取得用
-    //Image image;
-
-    //// 新しいスプライトをアタッチ
-    //[SerializeField] Sprite Unko;//我らがうんこ大王
-
-
     public RectTransform image; // 動かす画像の参照
     private int counter = 0; // カウンター変数
-    private float move = 10.0f; // 移動量（速度）
-    private float num = 1720;//端から端までの移動量
+    private float move =10.0f; //この数字の2倍動きます
+
+    private float num = 1600.0f;//端から端までの移動量
+
+    bool canmove = false;//波を表示してうごかすかどうか
 
 
     private void Start()
     {
-        //image = GetComponent<Image>();
-        //image.sprite = Unko;
+
     }
 
-    void Update()
+    void  FixedUpdate()
     {
-
-       
-
 
         // スペースキーが押されたらシーンを切り替える
         if (Input.GetKeyDown(KeyCode.Return))
         {
 
-            //image = GetComponent<Image>();
-            //image.sprite = Unko;
+            canmove = true;
 
+        }
 
-            // 画像を上下に動かす
+        if (canmove)
+        {
             image.position += new Vector3(-move, 0, 0);
             num -= move;
+        }
 
-            // カウンターが一定値に達したら方向を反転
-            if (num == 0)
-            {
-                SceneManager.LoadScene("SampleGameScene"); // "NextScene"は切り替え先のシーン名
-            }
 
-           
+        // カウンターが一定値に達したら方向を反転
+        if (num <= 0)
+        {
+            canmove = false;
+
+            SceneManager.LoadScene("SampleGameScene"); // "NextScene"は切り替え先のシーン名
         }
     }
 }
